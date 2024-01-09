62ad24645a90779de2b1cfba5bfe632c2c9bad68
Edwin Andermyr <andermyredwin@gmail.com>
2024-01-09 15:58:47 +0100

    initial repo

---
+++ ed.js
@@ -0,0 +1,15 @@
+$(() => {
+  SPOTIFY_URL = "https://api.spotify.com/v1/artists/";
+
+  async function getProfile(accessToken) {
+    let accessToken = localStorage.getItem("access_token");
+
+    const response = await fetch("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    });
+
+    const data = await response.json();
+  }
+});
